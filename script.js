d93d9ec9c09765d24db5adca8abc574cd079511d
MarHajduk <125913481+MarHajduk@users.noreply.github.com>
2023-03-08 22:41:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -8,31 +8,30 @@ const score_1 = document.getElementById(`score--1`);
 const current_0 = document.getElementById(`current--0`);
 const current_1 = document.getElementById(`current--1`);
 const winner = document.querySelector(`.winner`);
+const faq = document.querySelector(`.faq`);
 const main = document.querySelector(`main`);
+const dicePic = document.getElementById('dice--pic');
 
-let userName = `String`;
+let userName = `Player 2`;
 let computerScore = 0;
 let userScore = 0;
 let currentScore = 0;
 let playerTurn = true;
 let refresh;
+let roll;
+
+
+function sleep(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
 
 const changePlayer = function () {
+  if(roll != 1){dicePic.src = `dice-0.png`};
   player.forEach(plr => {
-    if(plr.classList.contains(`player--active`)){
-        plr.classList.remove(`player--active`);
-    } else {
-        plr.classList.add(`player--active`);
-    };
+  plr.classList.toggle(`player--active`)
   });
-  if(playerTurn){
-document.querySelector(`.btn--roll`).removeEventListener('click', diceRoll);
-document.querySelector(`.btn--roll`).removeEventListener('click', takeScore);
-document.querySelector(`.btn--hold`).removeEventListener('click', hold);} else{
-  document.querySelector(`.btn--roll`).addEventListener('click', diceRoll);
-  document.querySelector(`.btn--roll`).addEventListener('click', takeScore);
-  document.querySelector(`.btn--hold`).addEventListener('click', hold);
-}
+  if(playerTurn){computerTurn()}
 };
 
 let newGame = function(){
@@ -46,49 +45,77 @@ let newGame = function(){
     player[0].classList.remove(`player--active`);
     player[1].classList.add(`player--active`);
     playerTurn=true;
-    document.querySelector(`.btn--roll`).addEventListener('click', diceRoll);
-    document.querySelector(`.btn--roll`).addEventListener('click', takeScore);
-    document.querySelector(`.btn--hold`).addEventListener('click', hold);
     winner.style.display = `none`;
     main.classList.remove(`main-hider`);
+    clearInterval(refresh);
+    faq.style.display = `none`
 };
 
+let winTester = function () {
+  setTimeout(function(){  if (userScore >= 100) {
+    winner.textContent = `The winner is ${userName}`;
+    winner.style.display = `block`;
+    main.classList.add(`main-hider`);
+  }
+  if (computerScore >= 100) {
+    winner.textContent = `Computer has won!`;
+    winner.style.display = `block`;
+    main.classList.add(`main-hider`);
+  }} ,2000)
+
+};
+
+
 let diceRoll = function(){
-    let roll = Math.floor(Math.random() * 6) + 1;
-    switch (roll) {
-      case 1: document.getElementById('dice--pic').src = 'dice-1.png';
-        dice.classList.add(`dice--effect--red`);
-        setTimeout(function () {
-        dice.classList.remove(`dice--effect--red`);
-          }, 500);
+  roll = Math.floor(Math.random() * 6) + 1;
+  dicePic.src = `dice-${roll}.png`;
+  dice.classList.add(`dice--effect`);
+  setTimeout(function () {
+        dice.classList.remove(`dice--effect`);
+      }, 600);
+  if(roll===1){
+      clearInterval(refresh);
+      dice.classList.add(`dice--effect--red`);
+      setTimeout(function () {
+      dice.classList.remove(`dice--effect--red`);
+      }, 600);
       currentScore = 0;
       current_0.textContent = currentScore;
       current_1.textContent = currentScore;
-      if(!playerTurn){changePlayer(); playerTurn = true}
-      else{changePlayer(); playerTurn = false}
-        break;
-      case 2: document.getElementById('dice--pic').src = 'dice-2.png';
-      currentScore += 2;
-        break;
-      case 3: document.getElementById('dice--pic').src = 'dice-3.png';
-      currentScore += 3;
-        break;
-      case 4: document.getElementById('dice--pic').src = 'dice-4.png';
-      currentScore += 4;
-        break;
-      case 5: document.getElementById('dice--pic').src = 'dice-5.png';
-      currentScore += 5;
-        break;
-      case 6: document.getElementById('dice--pic').src = 'dice-6.png';
-      currentScore += 6;
-        break;
-    };
-    dice.classList.add(`dice--effect`);
-
-    setTimeout(function() {dice.classList.remove(`dice--effect`);}, 100);
-    
-    return roll
+      if(playerTurn){changePlayer(); playerTurn = false}
+      else{changePlayer(); playerTurn = true}
+    } else{currentScore += roll;}
 };
+
+let dicetest = function(){
+        dice.classList.add(`dice--effect--red`);
+        dice.classList.remove(`dice--effect--red`);
+};
+
+let hold = function () {
+  if(playerTurn){
+    userScore += currentScore;
+    winTester();
+    score_1.textContent = userScore;
+    currentScore = 0;
+    current_1.textContent = currentScore;
+    changePlayer();
+    playerTurn = false;
+}};
+
+let takeScore = function () {
+  if(playerTurn){
+  diceRoll();
+  current_1.textContent = currentScore;
+}};
+
+
+let renameBox = function () {
+  userName = document.getElementById(`name`).value
+  playerName.textContent = userName;
+};
+
+
 
 let compCycle_1 = function () {
   diceRoll();
@@ -96,59 +123,40 @@ let compCycle_1 = function () {
 };
 
 let compCycle_2_hold = function () {
-  computerScore += currentScore;
-  score_0.textContent = computerScore;
-  currentScore = 0;
-  current_0.textContent = currentScore;
-  changePlayer();
-  playerTurn = true;
-};
-
-
-refresh = setInterval(function () {
-    if(!playerTurn){
-    compCycle_1();
-    if(currentScore+computerScore>=100){compCycle_2_hold()}
-    if (currentScore > 18 && computerScore < 75 && userScore < 75) {
-      compCycle_2_hold();
-    }
-}
-  }, 1000);
-
-  let winTester = setInterval(function () {
-    if (userScore>=100) {
-        winner.textContent = `The winner is ${userName}`;
-        winner.style.display = `block`;
-        main.classList.add(`main-hider`)
-    }
-    if (computerScore >= 100) {
-        winner.textContent = `Computer has won!`;
-        winner.style.display = `block`;
-        main.classList.add(`main-hider`);}
-}, 500);
-
-let hold = function(){
-    userScore += currentScore;
-    score_1.textContent = userScore;
+  clearInterval(refresh);
+  
+  setTimeout(function () {  
+    computerScore += currentScore;
+    winTester()
+    score_0.textContent = computerScore;
     currentScore = 0;
-    current_1.textContent = currentScore;
+    current_0.textContent = currentScore;
     changePlayer();
-    playerTurn = false
+    playerTurn = true;
+  }, 1500);
+
 };
-let takeScore = function(){
-    current_1.textContent = currentScore;
+
+let computerTurn = function(){ 
+refresh = setInterval(function () {
+    compCycle_1();
+      if(currentScore + computerScore >= 100){compCycle_2_hold()}
+      if(currentScore >= 18 && computerScore < 30 && userScore < 50){compCycle_2_hold()}
+      if(currentScore >= 23 && computerScore < 60 && userScore < 75){compCycle_2_hold()}
+  }, 1000)};
+
+let instructions = function(){
+  main.classList.toggle(`main-hider`);
+  faq.style.display  == `block` ? faq.style.display = `none` : faq.style.display = `block`;
 }
+let enterKeypress = function(k){if(k.key === `Enter`){instructions()}}
+
 
 document.querySelector(`.btn--new`).addEventListener('click', newGame);
-document.querySelector(`.btn--roll`).addEventListener('click', diceRoll);
+document.querySelector(`.btn--faq`).addEventListener('click', instructions);
 document.querySelector(`.btn--roll`).addEventListener('click', takeScore);
 document.querySelector(`.btn--hold`).addEventListener('click', hold);
-
-let rename = function(){
-userName = prompt('Please enter your name');
-playerName.textContent = userName
-}
-playerName.addEventListener('click', rename);
-
+document.getElementById(`name`).addEventListener('input', renameBox);
+document.addEventListener('keydown', enterKeypress);
 
 
